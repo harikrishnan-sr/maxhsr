3949c41f2463329ba829f096dd8f10bf56f9fedc
harikrishnan-sr <30754304+harikrishnan-sr@users.noreply.github.com>
2023-07-13 15:00:33 +0530

    10 files modified, 2 added

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,28 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+    ),
+    new Recipe(
+      'Big Fat Burger',
+      'What else you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
+    ),
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -6,6 +6,7 @@ import {
   ViewChild,
 } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -15,10 +16,11 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
 export class ShoppingEditComponent {
   @ViewChild('nameInput') nameInputRef: ElementRef<HTMLInputElement>;
   @ViewChild('amountInput') amountInputRef: ElementRef<HTMLInputElement>;
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
+  constructor(private slService: ShoppingListService) {}
 
   onAddItem() {
-    this.ingredientAdded.emit(
+    this.slService.addIngredient(
       new Ingredient(
         this.nameInputRef.nativeElement.value,
         +this.amountInputRef.nativeElement.value
